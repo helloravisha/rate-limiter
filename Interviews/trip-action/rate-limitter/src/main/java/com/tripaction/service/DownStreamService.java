3b5219fac9bd394f70fc46fbc04be0e8daea8232
helloravisha <helloravisha@gmail.com>
2021-03-29 00:58:49 -0700

    rate limitter

--- Interviews/trip-action/rate-limitter/src/main/java/com/tripaction/service/DownStreamService.java
+++
@@ -1,7 +0,0 @@
-package com.tripaction.service;
-
-import com.tripaction.request.DownstreamRequest;
-
-public interface DownStreamService {
-    String invokeAPI(DownstreamRequest DownstreamRequest);
-}
